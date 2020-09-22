e1efbbd0635077a90198d6ea14885c8405ac64a8
Kaikai8888 <wuky812@gmail.com>
2020-09-22 12:57:27 +0800

    Feat: prioritizing todos by drag and drop

--- script.js
+++ script.js
@@ -61,3 +61,51 @@ main.addEventListener("click", function (event) {
     }
   }
 });
+
+//Prioritize(drag and drop):
+// reference source: https://pjchender.blogspot.com/2017/08/html5-drag-and-drop-api.html
+
+//drag source setting
+items.forEach((item) => addDragSetting);
+let dragItem;
+
+function addDragSetting(item) {
+  item.draggable = "true";
+  //drag source setting
+  item.addEventListener("dragstart", dragStart);
+  //drop target setting
+  item.addEventListener("drop", dropped);
+  item.addEventListener("dragenter", cancelDefault);
+  item.addEventListener("dragover", cancelDefault);
+}
+
+function dragStart(e) {
+  const index = $(e.target).index();
+  e.dataTransfer.setData("text/plain", index);
+  dragItem = this;
+}
+
+function dropped(e) {
+  cancelDefault(e);
+
+  //get old and new location index
+  const oldIndex = $(dragItem).index();
+  const newIndex = $(this).index();
+
+  const curItems = document.querySelectorAll("#my-todo > li");
+  if (dragItem.parentElement.id !== "done") {
+    if (oldIndex < newIndex) {
+      this.after(curItems[oldIndex]);
+    } else {
+      this.before(curItems[oldIndex]);
+    }
+  } else if (this.parentElement.id === "my-todo") {
+    dragItem.firstElementChild.classList.remove("checked");
+    this.after(dragItem);
+  }
+}
+
+function cancelDefault(e) {
+  e.preventDefault();
+  return false;
+}
